ad0f0556bbfd6c8d4b9c80d1d1bdca4a20f7f502
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2023-11-17 20:18:24 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import MapScreen from './screens'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <MapScreen/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/screens/MapScreen.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const MapScreen = () => {
+
+  return (
+    <>
+        <div>
+            <h1>Map</h1>
+        </div>
+    </>
+  );
+};
+
+export default MapScreen;
